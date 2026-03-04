faaf71b9c676f1f2b1dbf541fb581ed667a1f134
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-03-04 11:45:47 -0300

    nova secao app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import '../src/index.css';
 
-import Header from './components/Header';
+import Header from './components/Header.jsx';
+import Home from './components/Home.jsx';
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
 
   <Header />
+  <Home />
 
     </div>
   );
