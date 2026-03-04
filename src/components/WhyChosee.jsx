21ea32bd4aec5066510605bb5b17e353142eff73
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-03-04 15:59:56 -0300

    tirand oteste

--- src/components/WhyChosee.jsx
+++ src/components/WhyChosee.jsx
@@ -11,12 +11,12 @@ const WhyChosee = () => {
 
 
                 <div className="flexbox">
-                    <div className="divDaFlexBox">s</div>
+                    <div className="divDaFlexBox"></div>
 
                     <div className="divDaFlexBox">d</div>
 
                     <div className="divDaFlexBox">f</div>
-                    
+
                     <div className="divDaFlexBox">a</div>
                 </div>
 
