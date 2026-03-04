35de327d9f7b58cb8e64d3f6cf8b12fd043b7c52
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-03-04 13:32:30 -0300

    conteudo

--- src/components/DreamHouse.jsx
+++ src/components/DreamHouse.jsx
@@ -13,6 +13,8 @@ const DreamHouse = () => {
 
                 <div className="conteudoDreamHouse">
                         <h3>We Help You To Find Your Dream Home</h3>
+
+                        <p>From cozy cottages to luxurious estates, our dedicated team guides you through every step of the journey, ensuring your dream home becomes a reality</p>
                 </div>
 
     </div>
