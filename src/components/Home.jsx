8d9b8dba5c22dd05cfe315b649fd339dcf873a5c
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-03-04 11:54:52 -0300

    paragarafo

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,6 +8,8 @@ const Home = () => {
             <div className="conteudoEscrito">
                 <h1 className="tituloPrincipal">Find Your <br />  Dream Home</h1>
 
+                <p className="paragrafoPrincipal">Explore our curated selection of exquisite properties meticulously tailored to your unique dream home vision</p>
+
             </div>
 
             <div className="conteudoImagem">
