507aac745c6ddec5c55a68e210c641b9a6ec4b08
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-03-05 22:00:28 -0300

    cidade

--- src/components/Popularresidences.jsx
+++ src/components/Popularresidences.jsx
@@ -21,7 +21,7 @@ const Popularresidences = () => {
 
                     <ul className="listaIconeEnomeCidade">
                     <li><img src={imgMaps} alt="" /></li>
-                        <li></li>
+                        <li>San Francisco, California</li>
                     </ul>
 
             </div>
