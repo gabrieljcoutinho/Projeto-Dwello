89aca45df6b6866c5628f794230fbb13b93c6ca8
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-03-05 21:44:02 -0300

    erro digitacso

--- src/components/Popularresidences.jsx
+++ src/components/Popularresidences.jsx
@@ -4,7 +4,7 @@ import "../Css/PopularResidence.css"
 
 const Popularresidences = () => {
   return (
-    <div><h4 className="tituloResidenciasPopulares">Pur Populares residences</h4></div>
+    <div><h4 className="tituloResidenciasPopulares">Our Populares residences</h4></div>
   )
 }
 
