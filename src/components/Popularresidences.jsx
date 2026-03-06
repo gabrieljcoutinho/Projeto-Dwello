ed0e972a7f703085473c7bccc95bcf40f5f17069
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-03-05 21:45:05 -0300

    detalhe

--- src/components/Popularresidences.jsx
+++ src/components/Popularresidences.jsx
@@ -4,7 +4,12 @@ import "../Css/PopularResidence.css"
 
 const Popularresidences = () => {
   return (
-    <div><h4 className="tituloResidenciasPopulares">Our Populares residences</h4></div>
+    <div>
+
+        <h4 className="tituloResidenciasPopulares">Our Populares residences</h4>
+
+
+    </div>
   )
 }
 
